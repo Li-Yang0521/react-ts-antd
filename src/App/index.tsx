011df0ce76d3b2e5c91da08b92b321900b409d7c
Yang Li <592611595@qq.com>
2020-02-02 16:17:23 +0800

    feat:添加tslint

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,13 +2,13 @@
  * @Author: Yang Li
  * @Date: 2020-02-02 10:48:36
  * @Last Modified by: Yang Li
- * @Last Modified time: 2020-02-02 10:52:15
+ * @Last Modified time: 2020-02-02 16:17:00
  */
-import React from "react";
-import logo from "../Assets/images/logo.svg";
-import { Button } from "antd";
+import React from 'react';
+import logo from '../Assets/images/logo.svg';
+import { Button } from 'antd';
 
-import "./index.less";
+import './index.less';
 
 const App = () => {
   return (
@@ -18,7 +18,9 @@ const App = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
         <Button type="primary">Button</Button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
